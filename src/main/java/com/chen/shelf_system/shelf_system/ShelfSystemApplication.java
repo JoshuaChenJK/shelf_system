d4989781e3b5756e14fc7e892b76a55781be9db2
chen <919252469@qq.com>
2018-12-21 23:24:05 +0800

    Initial commit

---
+++ src/main/java/com/chen/shelf_system/shelf_system/ShelfSystemApplication.java
@@ -0,0 +1,14 @@
+package com.chen.shelf_system.shelf_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfSystemApplication.class, args);
+	}
+
+}
+
---
+++ src/main/resources/shema.sql
@@ -0,0 +1,13 @@
+# CREATE TABLE commodity(
+#   id INTEGER  AUTO_INCREMENT NOT NULL ,
+#   name VARCHAR(20) NOT NULL ,
+#   price INTEGER NOT NULL ,
+#   place VARCHAR(20),
+#   photo blob
+# )ENGINE = InnoDB DEFAULT CHARSET =utf8;
+#
+# INSERT INTO commodity VALUES (1,'华为 mate 20 pro',5399,'杭州',NULL);
+# INSERT INTO commodity VALUES (2,'毛衣 男',129,'上海',NULL);
+# INSERT INTO commodity VALUES (3,'鼠标',53,'深圳',NULL);
+# INSERT INTO commodity VALUES (4,'耳机',20,'四川',NULL);
+# INSERT INTO commodity VALUES (5,'木梳',120,'江苏',NULL );
